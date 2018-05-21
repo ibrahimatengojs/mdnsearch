fd40ae26b8918d302bba0a9618de6a1038c36e0d
Jonathan Kuhl <jonathankuhl@Jonathans-MBP.maine.rr.com>
2018-05-21 12:44:24 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# mdnsearch
+
+Searches the Mozilla Developer Network for a given argument.  Example:
+
+```
+mdn arrays
+```
+
+Will open a new search at the MDN for arrays.
+
+# Install
+
+```
+npm install -g mdnsearch
+```
---
+++ mdn.js
@@ -0,0 +1,9 @@
+const opn = require('opn');
+const search = process.argv[2];
+if(search) {
+    const url = `https://developer.mozilla.org/en-US/search?q=${search}`
+    opn(url);
+} else {
+    console.log("Please provide a parameter to search the MDN for");
+}
+process.exit(0);
